f2768291534af4b4e6d00a6ff30face2f441d5e8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-21 23:36:22 +0300

    no message

--- view/adminhtml/web/main.js
+++ view/adminhtml/web/main.js
@@ -6,20 +6,24 @@ require([
 		return /\d+\.\d+\.\d+\.\d+/.test(this.innerHTML);
 	});
 	if ($ipAddressCell.length) {
-		$.get('https://freegeoip.net/json/' + $ipAddressCell.html(), function(data) {
-			/** @type {String} */
-			var location =
-				[data['country_name'], data['region_name'], data['city']]
-					.filter(function(item) {return !!item;})
-					.join(', ')
-			;
-			/** @type {jQuery} HTMLTableRowElement */
-			var $ipAddressRow = $ipAddressCell.closest('tr');
-			var $geoRow = $('<tr>')
-				.append($('<th>').append($.mage.__('Customer Location')))
-				.append($('<td>').append(location))
-			;
-			$ipAddressRow.after($geoRow);
-		});
+		/** @type {String} */
+		var address = $ipAddressCell.html();
+		if ('127.0.0.1' !== address) {
+			$.get('https://freegeoip.net/json/' + address, function(data) {
+				/** @type {String} */
+				var location =
+					[data['country_name'], data['region_name'], data['city']]
+						.filter(function(item) {return !!item;})
+						.join(', ')
+				;
+				/** @type {jQuery} HTMLTableRowElement */
+				var $ipAddressRow = $ipAddressCell.closest('tr');
+				var $geoRow = $('<tr>')
+					.append($('<th>').append($.mage.__('Customer Location')))
+					.append($('<td>').append(location))
+				;
+				$ipAddressRow.after($geoRow);
+			});
+		}
 	}
 });});
